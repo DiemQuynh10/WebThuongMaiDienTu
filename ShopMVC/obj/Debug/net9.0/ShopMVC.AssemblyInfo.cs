d3b4164c9c3a0ef3e75e273ab5281b8d1adf088d
DauMai261204 <dauthimai2014@gmail.com>
2025-11-12 14:59:23 +0700

    Cập nhật code mới

--- ShopMVC/obj/Debug/net9.0/ShopMVC.AssemblyInfo.cs
+++ ShopMVC/obj/Debug/net9.0/ShopMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21880fed27c70970cd82236fc264e3efdb070802")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
